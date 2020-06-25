90fab375c10603c4a14ec323c4af60b45ef12160
ChandanChoudhary13 <65276032+ChandanChoudhary13@users.noreply.github.com>
2020-06-26 01:20:32 +0530

    #Redux | React-Redux | Redux-thunk Implementation
    Signed-off-by: ChandanChoudhary13 <65276032+ChandanChoudhary13@users.noreply.github.com>

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,77 +1,90 @@
 import React from 'react';
+import { connect } from "react-redux";
+import { handleInputAction } from "../myactions/action";
 
-class Home extends React.Component{
-  state={
-    text:'',
-    mywishes:[{_id:1,wish:"loading"}]
+class Home extends React.Component {
+  state = {
+    text: '',
+    mywishes: [{ _id: 1, wish: "loading" }]
   }
-  handledelete(id){
-    fetch('/remove/'+id,{method:"delete"})
-    .then(res=>res.json())
-    .then(res2=>{
-      console.log(res2)
-      const newWishes = this.state.mywishes.filter(item=>{
-        return item._id !== res2._id
+  handledelete(id) {
+    fetch('/remove/' + id, { method: "delete" })
+      .then(res => res.json())
+      .then(res2 => {
+        console.log(res2)
+        const newWishes = this.state.mywishes.filter(item => {
+          return item._id !== res2._id
+        })
+        this.setState({
+          mywishes: newWishes
+        })
       })
-      this.setState({
-        mywishes:newWishes
-      })
-    })
   }
-  componentDidMount(){
+  componentDidMount() {
     fetch('/data')
-    .then(res=>res.json())
-    .then(res2=>{
-      console.log(res2)
-      this.setState({
-        mywishes:res2
+      .then(res => res.json())
+      .then(res2 => {
+        console.log(res2)
+        this.setState({
+          mywishes: res2
+        })
       })
-    })
   }
-  handleSubmit(e){
-        e.preventDefault();
-       // const url = "http://localhost:5000/sent";
-        var data = new URLSearchParams();
-         for(const pair of new FormData(e.target)){
-           data.append(pair[0],pair[1])
-         }
-         //localhost:5000/sent
-        fetch('/sent',{
-            method:"post",
-            body:data,
-           
-        }).then(res=>res.json())
-        .then(res2 => {
-          console.log(res2)
-        
-          this.setState({
-            mywishes:[...this.state.mywishes,res2]
-          })
-        }); 
-  }
-  render(){
-     const list = this.state.mywishes.map(item=>{
-       return <a className="collection-item" key={item._id} onClick={()=>this.handledelete(item._id)}>{item.wish}</a>
-     })
-      return(
-        <div>
-          <form onSubmit={(e)=>this.handleSubmit(e)}>
-            <input 
-              type="text"
-              name="item"
-              value={this.state.text} 
-              onChange={(e)=>this.setState({text:e.target.value})}
-               />
-              <button type="submit" className="waves-effect waves-light btn">Add</button> 
-          </form>
+  handleSubmit(e) {
+    e.preventDefault();
+    // const url = "http://localhost:5000/sent";
+    var data = new URLSearchParams();
+    for (const pair of new FormData(e.target)) {
+      data.append(pair[0], pair[1])
+    }
+    //localhost:5000/sent
+    fetch('/sent', {
+      method: "post",
+      body: data,
 
-       <div className="collection">
-         {list}
-      </div>
-        </div>
-      )
+    }).then(res => res.json())
+      .then(res2 => {
+        console.log(res2)
+
+        this.setState({
+          mywishes: [...this.state.mywishes, res2]
+        })
+      });
   }
- 
+  render() {
+    const list = this.state.mywishes.map(item => {
+      return <a className="collection-item" key={item._id} onClick={() => this.handledelete(item._id)}>{item.wish}</a>
+    })
+    return (
+      <div>
+        <form onSubmit={(e) => this.handleSubmit(e)}>
+          <input
+            type="text"
+            name="item"
+            value={this.props.text}
+            onChange={(e) => this.props.handleinput(e.target.value)}
+          />
+          <button type="submit" className="waves-effect waves-light btn">Add</button>
+        </form>
+
+        <div className="collection">
+          {list}
+        </div>
+      </div>
+    )
+  }
+
 }
 
-export default Home;
+const mapStoreToProps = (state) => {
+  return {
+    text: state.text
+  }
+}
+const mapDispatchToProps = (dispatch) => {
+
+  return {
+    handleinput: (input) => { dispatch(handleInputAction(input)) }
+  }
+}
+export default connect(mapStoreToProps, mapDispatchToProps)(Home);
---
+++ client/src/myreducer/reducer.jsx
@@ -0,0 +1,18 @@
+const { STATES } = require("mongoose")
+
+const iState = {
+    text: '',
+    mywishes: [{ _id: 1, wish: "loading" }]
+}
+
+
+const reducer = (state = iState, action) => {
+    if (action.type === "UPDATE_INPUT") {
+        return {
+            ...state,
+            text: action.payload
+        }
+    }
+    return state;
+}
+export default reducer;
